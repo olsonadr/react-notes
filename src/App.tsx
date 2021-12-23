d5c2c8f05fb3d0bd83689b69862978344d1788ab
olsonadr <olsonnick10@yahoo.com>
2021-12-23 02:36:23 -0800

    Fixed ios fixed element scrolling issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
 import React, { useState } from "react";
+import styled from "@emotion/styled";
 import "./styles/App.css";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import MainPanel from "./components/MainPanel";
 
+// Styled elements
+const AppComp = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`;
+
+// Exported App react component
 function App() {
   const [sidebar, setSidebar] = useState(false);
 
   return (
-    <div className="App">
-      <Navbar
-        setSidebar={setSidebar}
-        sidebar={sidebar}
-        />
-      <Sidebar
-        setSidebar={setSidebar}
-        sidebar={sidebar} />
-      <MainPanel
-        setSidebar={setSidebar}
-        sidebar={sidebar}
-        />
-    </div>
+    <AppComp>
+      <Navbar setSidebar={setSidebar} sidebar={sidebar} />
+      <Sidebar setSidebar={setSidebar} sidebar={sidebar} />
+      <MainPanel setSidebar={setSidebar} sidebar={sidebar} />
+    </AppComp>
   );
 }
-
-
 
 export default App;
