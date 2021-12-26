033c85182d66b1dd9ac16db048232fed1e25cc44
olsonadr <olsonnick10@yahoo.com>
2021-12-25 23:33:40 -0800

    Added commented logging lines

--- server.ts
+++ server.ts
@@ -28,7 +28,10 @@ const PORT = process.env.PORT || process.env.REACT_APP_PORT || 3000;
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
     cors: {
-        origin: [`http://localhost:${PORT}`, 'react-notes-nerd.herokuapp.com'],
+        origin: [
+            `http://localhost:${PORT}`,
+            'react-notes-nerd.herokuapp.com'
+        ],
     }
 });
 
@@ -59,26 +62,27 @@ io.on('connection', (socket) => {
     // Handler for receiving user authentication message
     socket.on('profile_request', (msg) => {
         // Handle auth message here (if payload given)
-        console.log('Received profile request!');
+        // console.log('Received profile request...');
         if (msg && msg.email && msg.name && msg.user_id) {
+            // console.log(` for auth'd user ${msg.name} w/ supposed id ${msg.user_id}, checking if they are in the users database...`);
             let picture = msg.picture ? msg.picture : "https://i.ibb.co/k4zLTbW/176-1760995-png-file-svg-user-icon-free-copyright-transparent.jpg";
             checkNewUser(msg.email, msg.name, picture, msg.user_id, pool)
-            .then((data) => {
-                    console.log('Sending profile response!');
-                    socket.emit('profile_response', data);
-                });
-            }
-        });
-        
-        // Handler for disconnecting socket.io client
-        socket.on('disconnect', () => {
-            // Handle client disconnect here
+                .then((data) => {
+                        // if (data) console.log(` they belong in database, sending profile response to them!\n`);
+                        // else console.log(` they don't belong in database, sending them no data!\n`);
+                        socket.emit('profile_response', data);
+                    });
+        }
+    });
+
+    // Handler for disconnecting socket.io client
+    socket.on('disconnect', () => {
+        // Handle client disconnect here
         (() => { })(); // do nothing placeholder
     });
 });
 
 // Listen for socket.io and react requests on same port
-// io.listen(PORT, () => {
 http.listen(PORT, () => {
+    console.log(`Listening for react and socket requests on port ${PORT}\n`);
 });
-console.log(`Listening for react and socket requests on port ${PORT}`);
